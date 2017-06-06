346ae3989c6a6f049490ffd582f6fce97d29bd8f
Thiago S. Santos <thiagos.xsantos@gmail.com>
2017-06-06 14:54:28 -0300

    Adiciona driver com recuperção de falhas de conexão

---
+++ src/Model/Doctrine/DBAL/Driver/PDOMySql/Driver.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link      http://github.com/zetta-repo/zetta-doctrineutil for the canonical source repository
+ * @copyright Copyright (c) 2016 Zetta Code
+ */
+
+namespace Zetta\DoctrineUtil\Model\Doctrine\DBAL\Driver\PDOMySql;
+
+class Driver extends \Doctrine\DBAL\Driver\PDOMySql\Driver
+{
+
+    public function getReconnectExceptions()
+    {
+        return [
+            'SQLSTATE[HY000]: General error: 2006 MySQL server has gone away',
+            'SQLSTATE[HY000] [2002] php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known'
+        ];
+    }
+
+    public function shouldStall(\Exception $x)
+    {
+        return stristr($x->getMessage(), 'php_network_getaddresses') !== false;
+    }
+
+}
