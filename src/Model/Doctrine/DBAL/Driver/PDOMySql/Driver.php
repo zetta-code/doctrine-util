ca425bab33878ecaa6fab18ebff28a7166386a5f
Thiago S. Santos <thiagos.xsantos@gmail.com>
2017-10-17 14:21:29 -0300

    Adiciona error 2013 ao DriverMysql de reconecção

--- src/Model/Doctrine/DBAL/Driver/PDOMySql/Driver.php
+++ src/Model/Doctrine/DBAL/Driver/PDOMySql/Driver.php
@@ -13,6 +13,7 @@ class Driver extends \Doctrine\DBAL\Driver\PDOMySql\Driver
     {
         return [
             'SQLSTATE[HY000]: General error: 2006 MySQL server has gone away',
+            'SQLSTATE[HY000]: General error: 2013 Lost connection to MySQL server during query',
             'SQLSTATE[HY000] [2002] php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known'
         ];
     }
